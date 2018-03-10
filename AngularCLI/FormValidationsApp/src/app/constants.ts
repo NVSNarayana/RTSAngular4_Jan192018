81d565a79d1b7bb0f6e1bdc6b59f74da891f7e48
ch.narayana <ch.narayana@intlfcstone.com>
2018-03-10 21:45:21 +0530

    March 10, 2018: Added Angular5 syntax ppt

--- AngularCLI/FormValidationsApp/src/app/constants.ts
+++ AngularCLI/FormValidationsApp/src/app/constants.ts
@@ -4,11 +4,19 @@ export class Constants {
     static setFormValidations(): FormGroup {
         return new FormGroup({
             'ename': new FormControl(null, [Validators.required, Validators.minLength(5)]),
-            'age': new FormControl(null, [Validators.required]),
+            'age': new FormControl(null, [Validators.required, Constants.ageRageValidation]),
             'salary': new FormControl(),
             'address': new FormControl(null, [Validators.required]),
             'higherQualifiation': new FormControl(null, Validators.required)
         }, /*{ updateOn: "submit" }*/)//updateon default is change. we can applay change/blur/submit
     }
+    static ageRageValidation(ctrl: FormControl) {
+        if (+ctrl.value >= 20 && +ctrl.value <= 50) {
+            return null;
+        }
+        else {
+            return { agerange: true }
+        }
 
+    }
 }
